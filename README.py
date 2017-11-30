3c5b5b485067ede2549f0f1e63ee6d1132832cc8
1-21-01-22B-08\pc1-21-01-22B-03 <sergio.sigcha@epn.edu.ec>
2017-11-30 09:50:03 -0500

    no message

--- README.py
+++ README.py
@@ -1,1 +1,16 @@
 # Ejercicio3.1
+def calculaCirculo():
+    radio=float(input("Ingrese el radio del circulo: "))
+    area=3.1416*radio*radio
+    print("el area del circulo es: ",area)
+
+w='x'
+while w=='x':
+    opcion = input("Escoja la opcion que desea calcular el area: ")
+    if opcion=='1':
+        calculaCirculo()
+    if opcion=='2':
+        print("")
+    if opcion=='3':
+        print("")
+    w=input("presione x para continuar cualquier tecla para salir: ")
