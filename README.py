684749c255690333752ac977568928e4258b7bd1
1-21-01-22B-07\pc1-21-01-22B-03 <bryan.corrales@epn.edu.ec>
2017-11-30 09:51:55 -0500

    area triangulo

--- README.py
+++ README.py
@@ -1,1 +1,6 @@
 # Ejercicio3.1
+def areatriangulo():
+    altura=int(input("Ingrese la altura: "))
+    base=int(input("Ingrese la base: "))
+    area=(base+altura)/2
+    print("La area del triangulo es: ",area)
